4756ab8b2ab59e8d359b8d30078dfabbba1a3df1
RAJESH KUMAR ATTRI <nitin@RAJESHs-MacBook-Air.local>
2026-06-01 23:32:22 +0530

    Added Merge Two Sorted Linked Lists Java solution

--- MergeTwoSortedLL.java
+++ MergeTwoSortedLL.java
@@ -1,5 +1,6 @@
 
 class ListNode {
+
     int val;
     ListNode next;
 
@@ -9,15 +10,17 @@ class ListNode {
     }
 }
 
-public class Main {
+public class MergeTwoSortedLL {
 
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        if (list1 == null)
+        if (list1 == null) {
             return list2;
+        }
 
-        if (list2 == null)
+        if (list2 == null) {
             return list1;
+        }
 
         if (list1.val < list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
@@ -38,12 +41,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-       
         ListNode list1 = new ListNode(1);
         list1.next = new ListNode(3);
         list1.next.next = new ListNode(5);
 
-    
         ListNode list2 = new ListNode(2);
         list2.next = new ListNode(4);
         list2.next.next = new ListNode(6);
