b9adda497a6b70ac21f5aa1396d899cf15a972e5
RAJESH KUMAR ATTRI <nitin@RAJESHs-MacBook-Air.local>
2026-06-01 23:29:55 +0530

    Added Merge Two Sorted Linked Lists Java solution

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Main {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null)
+            return list2;
+
+        if (list2 == null)
+            return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+       
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+    
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        ListNode result = mergeTwoLists(list1, list2);
+
+        System.out.println("Merged Linked List:");
+        printList(result);
+    }
+}
